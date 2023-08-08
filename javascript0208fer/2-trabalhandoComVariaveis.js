dc9bba9907635d2e641cf63b9fcdbdb032c71644
ceepfer <126881890+ceepfer@users.noreply.github.com>
2023-08-08 11:04:40 -0300

    Add files via upload

---
+++ javascript0208fer/2-trabalhandoComVariaveis.js
@@ -0,0 +1,9 @@
+//Console.log("Trabalhando com varíaveis");
+console.log("Trabalhando com varíavel");
+//JS é case sensetive
+
+idade = 15;
+const nome = "Fernanda";
+
+// não fazer
+//mês = "Outubro";
---
+++ javascript0208fer/5-atribuicaoVariaveis.js
@@ -0,0 +1,17 @@
+console.log("Trabalhando com atribuição de variáveis");
+const idade = 15;
+const nome = "Fernanda";
+const sobrenome = "Benites";
+
+//console.log(nome + "  " + sobrenome);
+console.log(nome, sobrenome);
+console.log('Meu nome é ${nome} ${sobrenome}');
+
+let contador = 0;
+contador = contador +1;
+
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
+
+let idade; //declarando variável
+idade =16
